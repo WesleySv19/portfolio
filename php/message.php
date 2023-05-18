e632ed40c0585b1cc54b4b59a606ac68aaf39cac
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-05-18 17:58:46 -0300

    tweacks with php

--- php/message.php
+++ php/message.php
@@ -1,33 +1,36 @@
 <?php 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['msg'];
-    $send_date = date('d/m/Y');
-    $send_hour = date('H:i:s');
+  if($_SERVER["REQUEST_METHOD"] === 'POST') {
+    header('location: http://localhost/myPortfolio/php/message_sent.php');
+  }
+  //   $name = $_POST['name'];
+  //   $email = $_POST['email'];
+  //   $message = $_POST['msg'];
+  //   $send_date = date('d/m/Y');
+  //   $send_hour = date('H:i:s');
 
-    // Campo E-mail
-    $file = "
-    <html>
-        <p><strong>Nome:</strong> $name</p>
-        <p><strong>E-mail:</strong> $email</p>
-        <p><strong>Mensagem:</strong> $message</p>
-        <p>Esse e-mail foi enviado em<strong>$send_date</strong>Às<strong>$send_hour</strong></p>
-    </html>
+  //   // Campo E-mail
+  //   $file = "
+  //   <html>
+  //       <p><strong>Nome:</strong> $name</p>
+  //       <p><strong>E-mail:</strong> $email</p>
+  //       <p><strong>Mensagem:</strong> $message</p>
+  //       <p>Esse e-mail foi enviado em<strong>$send_date</strong>Às<strong>$send_hour</strong></p>
+  //   </html>
     
-    ";
+  //   ";
 
-    // Para quem o email será enviado
-    $destiny = 'wesleyysilvaa1912@gmail.com';
-    $subject = 'Contato pelo site.';
+  //   // Para quem o email será enviado
+  //   $destiny = 'wesleyysilvaa1912@gmail.com';
+  //   $subject = 'Contato pelo site.';
 
     
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $name <$email>";
+  // //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  // $headers  = "MIME-Version: 1.0\n";
+  // $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  // $headers .= "From: $name <$email>";
 
-  //Enviar
-  mail($destiny, $subject, $file, $headers);
+  // //Enviar
+  // mail($destiny, $subject, $file, $headers);
   
-  echo "<meta http-equiv='refresh' content='10;URL=../contact.html'>";
+  // echo "<meta http-equiv='refresh' content='10;URL=../contact.html'>";
 ?>
