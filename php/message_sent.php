28a00aef73e3fe03d0ff4c1019d82a4f89173cdb
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-05-18 17:59:56 -0300

    new file php

---
+++ php/message_sent.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <link rel="icon" type="image/png" href="img/programmerIcon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Wesley-da-Silva</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div id="logo">
+                <a href="index.html"><img class="logoProgrammer" src="../img/programmer.png" height="70"></a>
+            </div>
+            <div id="nav-target" class="collapse navbar-collapse">
+                <ul class="nav-list">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">Sobre</a></li>
+                    <li><a href="projects.html">Projetos</a></li>
+                    <li><a href="#" class="active">Contato</a></li>
+                </ul>
+            </div>
+            <button id="btnTarget" class="navbar-toggler" data-toggle="collapse" data-target="#nav-target">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+        </nav>
+    </header>
+
+    <main>
+        <div class="contentForm" id="contactForm">
+            <div class="contentForm1">
+                <h1><span class="spanColor">{</span>Mensagem Enviada com sucesso!<span class="spanColor">}</span></h1>
+                <img class="animate__animated animate__zoomInUp" src="../img/verificado.png" height="200">
+                <!-- <p class="text"></p> -->
+            </div>
+
+        </div>
+    </main>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
+
+
