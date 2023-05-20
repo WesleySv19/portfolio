f505e6a13b10074d84da71d80195f0b26c49efb6
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-05-20 10:01:46 -0300

    edit php

--- php/send_email.php
+++ php/send_email.php
@@ -1,34 +1,35 @@
 <?php
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+    ini_set('SMTP', 'smtp.gmail.com');
+    ini_set('smtp_port', 587);
+    ini_set('sendmail_from', 'wesleyysilvaa1912@gmail.com');
 
-    require '../lib/phpMailer/Exception.php';
-    require '../lib/phpMailer/PHPMailer.php';
-    require '../lib/phpMailer/SMTP.php';
+ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+    // Dados vindos do formulário
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['msg'];
 
-    echo $email = $_POST['email'];  
-    echo $name = $_POST['name'];
-    echo $subject = $_POST['subject'];
-    echo $message = $_POST['msg'];
-  
-if ($_SERVER["REQUEST_METHOD"] === 'POST') {
-    $mail = new PHPMailer(true);
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->IsHTML(true); 
-    $mail->Port = 587;
+    $body = "<strong>Assunto:</strong><h2> $subject</h2>
+             <p><strong>Nome:</strong> $name</p>
+             <p><strong>Mensagem:</strong> $message</p>";
 
 
-    $mail->addAddress($email, 'Wesley');
-    $mail->Subject = $_POST['subject'];
-    $mail->Body = $_POST['msg'];
+    $to = "wesleyysilvaa1912@gmail.com";
 
-    if(!$mail->Send())
-    echo "Erro ao enviar Email:" . $mail->ErrorInfo;
-
+    if (mail($to, $body, $headers)) {
+        echo 'Mensagem enviada com sucesso!';
+    } else {
+        echo 'Ocorreu um erro ao enviar a mensagem.';
+    }
 }
+
 
 
 ?>
